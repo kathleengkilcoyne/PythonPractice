d697ac5ba043d8fe43fdac2d6c47e0a08229d78e
Katie Kilcoyne <35579655+kathleengkilcoyne@users.noreply.github.com>
2022-02-07 18:24:10 -0500

    Project Description and comments added

--- groupbyowners/groupbyowners.py
+++ groupbyowners/groupbyowners.py
@@ -1,6 +1,13 @@
+#This test was provided by Test Dome as a Python employment test
+#Test was acquired here: https://app.testdome.com/t?GeneratorId=46
+#Implement a group_by_owners function that:
+#1) accepts a dictionary containing the file owner name for each file
+#2) returns a dictionary containing a list of file names for each owner name, in any order.
+#For example, for dictionary {'input.txt':'Randy', 'code.py':'Stan', 'Output.txt':'Randy'}
+#the group_by_owners function should return {'Randy':['input.txt',''output.txt'], 'stan':['Code.py']}
 def group_by_owners(files):
+    #create empty dictionary
     v = {}
-
     for key, value in sorted(files.items()):
         v.setdefault(value, []).append(key)
     return v
